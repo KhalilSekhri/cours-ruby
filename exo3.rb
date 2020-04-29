eff7fbfdfe0f149fb197746c1059513c32e0efb3
KhalilSekhri <khalil.sekhri@gmail.com>
2020-04-29 12:05:52 +0200

    ajout de réponses partiel aux exo2 et 3

--- exo3.rb
+++ exo3.rb
@@ -1,3 +1,18 @@
-first_number = ARGV[0].to_i
-second_number = ARGV[1].to_i
-puts first_number + second_number
+tableau = []
+
+for i in 1..5
+  puts "Ecrivez un nomre : #{i}"
+  tableau += [gets.chomp.to_i]
+end
+
+plusGrand = 0
+for i in 1..(tableau.length-1)
+
+  if tableau[i]>tableau[plusgrand]
+    plusGrand = i
+  end
+
+end
+
+puts "Le nombre le plus grand est : [#{plusGrand}] => #{tableau[plusGrand]}"
+puts "La moyenne de tout les nombre est "
